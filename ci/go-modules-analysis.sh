2c274a859c166f5d61ac5ac9d550f6e98e0b7c29
Zakaria Guennoune <83596451+zguennoune02@users.noreply.github.com>
2022-04-05 09:04:53 +0200

    init(ci): Initialise pipeline with sonarqube call (#1)
    * Create Jenkinsfile
    
    * Update Jenkinsfile
    
    * Create go-modules-analysis.sh
    
    * permissions
    
    * permissions

---
+++ ci/go-modules-analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# $1 is the sonar.login token
+
+set -e
+
+# This job is run directly after mon-web-unittest on centos7.
+VERSION=0.1
+
+if [ -z "$VERSION" ] ; then
+  echo "You need to specify the VERSION variable"
+  exit 1
+fi
+
+sonar-scanner -Dsonar.projectVersion="$VERSION" -Dsonar.login="$1" -Dsonar.host.url="$2"
